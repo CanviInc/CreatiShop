46f0ac5d07fb0af74884a989cfed6047525cd76d
ankurbajad <ankurbajad786@gmail.com>
2014-11-26 19:40:23 +0530

    new user create by admin

--- app/models/admin/manager.rb
+++ app/models/admin/manager.rb
@@ -4,7 +4,7 @@ class Admin::Manager < ActiveRecord::Base
   self.table_name = "admin_managers"
   #attr_accessible :manager_email, :manager_name, :manager_password, :manager_role, :salt
 
-  validates :manager_name, :presence => true , :length => { :maximum => 50 } 
+  validates :manager_name, :presence => true, uniqueness: true , :length => { :maximum => 50 } 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :manager_email, :presence => true, :format => { :with => email_regex }, :uniqueness => { :case_sensitive => false }  
   validates :manager_password, :presence => true, :confirmation => true, :length => {:within => 6..40}
@@ -12,8 +12,8 @@ class Admin::Manager < ActiveRecord::Base
   before_save :encrypt_password
 
   def self.authenticate(username_or_email="", login_password="")    
-      manager = Admin::Manager.find_by_manager_name(username_or_email)
-
+    # manager = Admin::Manager.find_by_manager_name(username_or_email)
+    manager = Admin::Manager.where(["lower(manager_name) = :value OR lower(manager_email) = :value", { :value => username_or_email.downcase }]).first
     if manager && manager.manager_password == Digest::SHA2.hexdigest(login_password)
       return manager
     else
